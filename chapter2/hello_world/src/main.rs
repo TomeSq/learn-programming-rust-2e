45967fc2346fec25e79b720348518fe278162c69
KenichiShintome <tome119+git@gmail.com>
2025-05-11 11:41:02 +0900

    main関数を修正し、引数から最大公約数を計算する機能を追加

--- chapter2/hello_world/src/main.rs
+++ chapter2/hello_world/src/main.rs
@@ -1,7 +1,29 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    // sikip1はプログラム名なのでスキップ
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    //引数がない場合はusageを表示して終了
+    if numbers.is_empty() {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d)
 }
 
+// ユークリッドの互除法を使った最大公約数（Greatest Common Divisor: GCD）を計算するアルゴリズム
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     while m != 0 {
         let t = m;
